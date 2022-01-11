4c04f0eacfd3222a1ac313aefaa4afde7f0dee85
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-01-10 20:24:30 -0700

    Add commentary

--- variable.js
+++ variable.js
@@ -1,9 +1,18 @@
+// STRINGS
+// These are three strings I'm defining.
+
 var name = "Steph";
 var favoriteColor = "orange";
 var petsName = "Stout"
+
+// NUMBERS
+
 var age = 24
 var numberOfPets = 1
 var numberOfSiblings = 2
+
+// BOOLEANS
+
 var isHungry = false
 var isSleepy = false
 var isTall = false
